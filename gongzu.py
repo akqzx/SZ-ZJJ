b87b92e10258f881cf12f135ae80f496dfc214f7
akqzx <110948590@qq.com>
2022-05-26 11:03:47 +0800

    update

--- gongzu.py
+++ gongzu.py
@@ -13,9 +13,9 @@ from openpyxl.chart import (
     LineChart,
     Reference,
 )
-beianhao = "备案回执号"
-xingming = "申请人(共同申请人)姓名"
-shenfenID = "申请人(共同申请人)身份证"
+gongzubeianhao = ""
+xingming = ""
+shenfenID = ""
 
 platform_ = platform.system()
 if platform_ == "Windows":
@@ -30,46 +30,58 @@ elif platform_ == "Linux":
 elif platform_ == "Mac":
     print("not support")
     sys.exit()
-if not os.path.exists("zjj.xlsx"):
+if not os.path.exists("gongzu.xlsx"):
     wb = Workbook()
     ws = wb.active
     ws.title = 'Sheet1'
     ws['A1'] = '时间'
-    ws['B1'] = '户籍区排序号'
-    ws['C1'] = '轮候排序号'
-    wb.save('zjj.xlsx')
+    ws['B1'] = '公租户籍区排序号'
+    ws['C1'] = '公租轮候排序号'
+    wb.save('gongzu.xlsx')
+###公租
 driver.get('https://zjj.sz.gov.cn/zfxx/bzflh/?path=main/#/lhmcgr')
-time.sleep(2)
+time.sleep(1)
 driver.find_element(By.XPATH, '//*[@id="app"]/section/header/form/div[1]/div/div/div/input').click()
-time.sleep(2)
+time.sleep(1)
 driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[1]/ul/li[2]').click()
-driver.find_element(By.XPATH, '//*[@id="app"]/section/header/form/div[2]/div/div/input').send_keys(beianhao)
+driver.find_element(By.XPATH, '//*[@id="app"]/section/header/form/div[2]/div/div/input').send_keys(gongzubeianhao)
 driver.find_element(By.XPATH, '//*[@id="app"]/section/header/form/div[3]/div/div/input').send_keys(xingming)
 driver.find_element(By.XPATH, '//*[@id="app"]/section/header/form/div[4]/div/div/input').send_keys(shenfenID)
 driver.find_element(By.XPATH, '//*[@id="app"]/section/header/form/div[5]/div/button[1]/span').click()
 time.sleep(3)
-qu = driver.find_element(By.XPATH, '//*[@id="app"]/section/main/div/div[3]/table/tbody/tr/td[2]').text
-shi = driver.find_element(By.XPATH, '//*[@id="app"]/section/main/div/div[3]/table/tbody/tr/td[3]').text
-wb = openpyxl.load_workbook('zjj.xlsx')
+gongzuqu = driver.find_element(By.XPATH, '//*[@id="app"]/section/main/div/div[3]/table/tbody/tr/td[2]').text
+gongzushi = driver.find_element(By.XPATH, '//*[@id="app"]/section/main/div/div[3]/table/tbody/tr/td[3]').text
+
+###查询上一次排位信息
+wb = openpyxl.load_workbook('gongzu.xlsx')
 ws = wb['Sheet1']
+oldgongzuqudyg = ("B"+str(ws.max_row))
+oldgongzuqu = ws[oldgongzuqudyg].value
+
+###判断是否与上一次数值相同
+if int(gongzuqu) == oldgongzuqu:
+    print("与上次查询的数据一致，结束")
+    sys.exit(0)
+
 line = str(ws.max_row+1)
 todyA = ("A"+line)
-qub = ("B"+line)
-shic = ("C"+line)
+gongzuqub = ("B"+line)
+gongzushic = ("C"+line)
 ws[todyA] = datetime.datetime.now().strftime("%Y-%m-%d")
-ws[qub] = float(qu)
-ws[shic] = float(shi)
+ws[gongzuqub] = float(gongzuqu)
+ws[gongzushic] = float(gongzushi)
 copy_sheet1 = wb.copy_worksheet(wb.worksheets[0])
 copy_sheet1.title = "Sheet2"
 del wb["Sheet1"]
 ws = wb["Sheet2"]
 ws.title = 'Sheet1'
 ws.column_dimensions['A'].width = 10.5
-ws.column_dimensions['B'].width = 12.5
-ws.column_dimensions['C'].width = 9.5
+ws.column_dimensions['B'].width = 16
+ws.column_dimensions['C'].width = 14
+
 data = Reference(ws, min_col=2, min_row=1, max_col=2, max_row=line)
 c1 = LineChart()
-c1.title = "户籍区排序号"
+c1.title = "公租户籍区排序号"
 c1.style = 14
 c1.y_axis.crossAx = 500
 c1.x_axis = DateAxis(crossAx=100)
@@ -79,9 +91,10 @@ c1.add_data(data, titles_from_data=True)
 dates = Reference(ws, min_col=1, min_row=2, max_row=line)
 c1.set_categories(dates)
 ws.add_chart(c1, "E3")
+
 data2 = Reference(ws, min_col=3, min_row=1, max_col=3, max_row=line)
 c2 = LineChart()
-c2.title = "轮候排序号"
+c2.title = "公租轮候排序号"
 c2.style = 12
 c2.y_axis.crossAx = 500
 c2.x_axis = DateAxis(crossAx=100)
@@ -91,6 +104,7 @@ c2.add_data(data2, titles_from_data=True)
 dates2 = Reference(ws, min_col=1, min_row=2, max_row=line)
 c2.set_categories(dates2)
 ws.add_chart(c2, "E20")
-wb.save('zjj.xlsx')
+
+wb.save('gongzu.xlsx')
 driver.close()
 driver.quit()
